9b31e9d2effe9798cbc42e916c5244d8c22039e4
Eduardo4d4 <eduardo.tapia4d4@gmail.com>
2023-06-18 03:55:29 -0600

    PHP final
    PHP, genera reportes sencillos y resumen de la tabla, guarda en txt y entrega
    registro posible desde cualquier pagina

--- pedidos.php
+++ pedidos.php
@@ -3,7 +3,7 @@
 $servername = "localhost";
 $username = "root";
 $password = "Mi4353fe";
-$dbname = "clientes";
+$dbname = "colegio";
 
 // Crear conexión
 $conn = new mysqli($servername, $username, $password, $dbname);
@@ -16,17 +16,61 @@ if ($conn->connect_error) {
 // Obtener los datos del formulario
 $nombre = $_POST["nombre"];
 $correo = $_POST["correo"];
-$comentario = $_POST["comentario"];
+$nivel = $_POST["nivel"];
+$opciones = isset($_POST["opcion"]) ? $_POST["opcion"] : array();
+$pago = $_POST["pago"];
+
+//Serializa los valores seleccionados en una cadena JSON
+$opciones_serializadas = json_encode($opciones);
 
 // Preparar la consulta
-$sql = "INSERT INTO client (nombre, correo, comentario) VALUES (?, ?, ?)";
+$sql = "INSERT INTO client (nombre, correo, nivel, opciones, pago) VALUES (?, ?, ?, ?, ?)";
 $stmt = $conn->prepare($sql);
-$stmt->bind_param("sss", $nombre, $correo, $comentario);
+$stmt->bind_param("sssss", $nombre, $correo, $nivel, $opciones_serializadas, $pago);
 
 // Ejecutar la consulta preparada
 if ($stmt->execute()) {
-    header("Location: index.html");
+    // Mensaje de agradecimiento
+    echo "Gracias por enviar el formulario. Nos pondremos en contacto contigo pronto. <br> <a href='index.html'>Regresar</a></center>";
 
+    // Consulta para obtener el estado actual de la tabla
+    $selectSql = "SELECT * FROM client";
+    $result = $conn->query($selectSql);
+
+    // Crear contenido del archivo
+    $content = "";
+
+    // Mostrar el estado actual de la tabla
+    if ($result->num_rows > 0) {
+        $content .= "Estado actual de la tabla client:\n\n";
+        $content .= "--------------------------------------------------------------------------------------------------------------------------------------\n";
+        $content .= "| Nombre        	    | Nivel    		   | Pago    		  | Correo                    | Opciones             |\n";
+        $content .= "--------------------------------------------------------------------------------------------------------------------------------------\n";
+        while ($row = $result->fetch_assoc()) {
+            $content .= "| " . str_pad($row["nombre"], 25, " ") . " | " . str_pad($row["nivel"], 20, " ") . " | " . str_pad($row["pago"], 20, " ") . " | " . str_pad($row["correo"], 25, " ") . " | " . str_pad($row["opciones"], 20, " ") . " |\n";
+        }
+        $content .= "--------------------------------------------------------------------------------------------------------------------------------------\n\n";
+
+        // Enunciado personalizado para cada registro
+        $content .= "Reportes:\n\n";
+        $content .= "Nombre\tCorreo\tNivel\tOpciones\tPago\n";
+        $result->data_seek(0); // Reiniciar el puntero del resultado
+        while ($row = $result->fetch_assoc()) {
+            $content .= $row["nombre"] . "\t" . $row["correo"] . "\t" . $row["nivel"] . "\t" . $row["opciones"] . "\t" . $row["pago"] . "\n";
+            $content .= "El usuario llamado " . $row["nombre"] . " y correo " . $row["correo"] . " desea inscribirse al nivel " . $row["nivel"] . " con una forma de pago " . $row["pago"] . " y desea recibir en su correo: " . $row["opciones"] . ".\n\n";
+        }
+
+        // Guardar contenido en un archivo de texto
+        $filename = "estado_clientes.txt";
+        file_put_contents($filename, $content);
+
+        // Descargar el archivo
+        echo "<br> <a href='$filename' download> Descargar Reporte</a>";
+    } else {
+        echo "La tabla cliente está vacía.";
+    }
+
+    exit;
 } else {
     echo "Error al ingresar los datos: " . $stmt->error;
 }
