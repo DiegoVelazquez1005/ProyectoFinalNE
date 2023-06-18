4af283b40079dcb0fa5f1125769bc118fb54ad63
Eduardo4d4 <eduardo.tapia4d4@gmail.com>
2023-06-17 22:36:08 -0600

    add imageMap+ js fun
    Image Map completa
    Función JS para cambio de texto x texto
    Mod de Ventana Flotante

--- script.js
+++ script.js
@@ -7,3 +7,28 @@ function mostrarPersona(imagen, texto) {
   document.getElementById('ventanaTexto').textContent = texto;
   $('#ventanaPersona').modal('show');
 }
+
+function alternarTexto(texto1, texto2) {
+  var parrafo = document.getElementById('P_Equipo');
+  if (parrafo.textContent === texto1) {
+    parrafo.textContent = texto2;
+  } else {
+    parrafo.textContent = texto1;
+  }
+}
+function alternarTexto2(texto1, texto2) {
+  var parrafo = document.getElementById('P_Equipo2');
+  if (parrafo.textContent === texto1) {
+    parrafo.textContent = texto2;
+  } else {
+    parrafo.textContent = texto1;
+  }
+}
+function alternarTexto3(texto1, texto2) {
+  var parrafo = document.getElementById('P_Equipo3');  
+  if (parrafo.textContent === texto1) {
+    parrafo.textContent = texto2;
+  } else {
+    parrafo.textContent = texto1;
+  }
+}
